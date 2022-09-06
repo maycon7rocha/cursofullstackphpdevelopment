86bb7f5616dbf6f5b3f5f3ecb346128d8ad4fc6f
Maycon Rocha <maycon7rocha@gmail.com>
2022-09-06 11:03:28 -0300

    aula 3.5

--- 03-manipulacao-e-tratamento/03-05-manipulacao-de-datas/index.php
+++ 03-manipulacao-e-tratamento/03-05-manipulacao-de-datas/index.php
@@ -9,8 +9,31 @@ fullStackPHPClassName("03.05 - Manipulação de datas");
  */
 fullStackPHPClassSession("a função date", __LINE__);
 
+var_dump([
+    date_default_timezone_get(),
+    date(DATE_W3C),
+]);
+echo ("<hr>");
 
+var_dump(getdate());
+
+echo "<p>Hoje é dia ", getdate()["mday"], "!</p>";
 /**
  * [ string to date ] strtotime | strftime
  */
 fullStackPHPClassSession("string to date", __LINE__);
+
+define("DATE_BR", "d/m/Y h:i:s");
+
+var_dump([
+    "strtotime NOW" => strtotime("now"),
+    "time" => time(),
+    "strtotime+10d" => strtotime("+10days"),
+    "date DATE_BR" => date(DATE_BR),
+    "date +10days" => date(DATE_BR, strtotime("+10days")),
+    "date -10days" => date(DATE_BR, strtotime("-10days")),
+    "date -1years" => date(DATE_BR, strtotime("-1years")),
+]);
+
+$format = "%d/%m/%Y %H horas e %M minutos";
+echo "<p>A data de hoje é ", strftime($format),"</p>";
